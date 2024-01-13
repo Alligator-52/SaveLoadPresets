24f8cc7e171307a88779888f3b0cdd580fc8a3e3
Alligator-52 <ayushankitpatnaik99@gmail.com>
2024-01-14 00:26:34 +0530

    Updated RuntimeSave class

--- AlligatorUtils/Scripts/Utils/Editor/RuntimeSave.cs
+++ AlligatorUtils/Scripts/Utils/Editor/RuntimeSave.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using Unity.VisualScripting;
+using System.IO;
+using System.Threading.Tasks;
 
 public class RuntimeSave : Editor
 {
     //private static RuntimeSave _runtimeSave;
     private static GameObject _saveObject;
+    private static string _savePath = Application.dataPath + "/TempObjects/";
 
     [MenuItem("GameObject/Custom Utilities/Save Game Object")]
     public static void SaveGameObject()
@@ -19,13 +22,31 @@ public class RuntimeSave : Editor
 
         GameObject currentObject = (GameObject)activeSelection;
         
-        if (EditorApplication.isPlaying)
+        if (!EditorApplication.isPlaying)
         {
             Debug.Log("We are here now");
+
+            //await CreateDirectory();
             _saveObject = currentObject;
-            Debug.Log($"save object : {_saveObject}");
+            if (AssetDatabase.IsValidFolder(_savePath))
+            {
+                AssetDatabase.CreateFolder("Assets", "TempObjects");
+            }
+            PrefabUtility.SaveAsPrefabAsset(_saveObject, _savePath); //+ currentObject.name + ".prefab");
+            //AssetDatabase.CreateAsset(_saveObject, Application.dataPath + _saveObject.name + ".prefab");
+            //Debug.Log($"save object : {_saveObject}");
         }
 
+    }
+
+    private static async Task CreateDirectory()
+    {
+        Debug.Log("Creating Directory");
+        if (!Directory.Exists(_savePath))
+        {
+            Directory.CreateDirectory(_savePath);
+        }
+        await Task.Yield();
     }
 
     [MenuItem("GameObject/Custom Utilities/Load Game Object")]
@@ -44,16 +65,5 @@ public class RuntimeSave : Editor
             _saveObject = null;
         }
 
-    }
-
-    public GameObject GetActiveGameObject()
-    {
-        var activeSelection = Selection.activeObject;
-        if (activeSelection.GetType() != typeof(GameObject))
-            return null;
-
-        var activeGameObj = (GameObject)activeSelection;
-
-        return activeGameObj;
     }
 }
